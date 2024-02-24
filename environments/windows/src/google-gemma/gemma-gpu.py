cdba0c405113ae683f80bbc2d9bca0ec7691a285
obriensystems <michael@obrienlabs.org>
2024-02-24 13:18:31 -0500

    #13 - add timing - multigpu depends on pcie or nvlink = 2x

--- environments/windows/src/google-gemma/gemma-gpu.py
+++ environments/windows/src/google-gemma/gemma-gpu.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from datetime import datetime
 
 access_token='hf_cfTP...XCQqH'
 
@@ -6,7 +7,13 @@ tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b", token=access_token)
 model = AutoModelForCausalLM.from_pretrained("google/gemma-7b", device_map="auto", token=access_token)
 
 input_text = "how is gold made in collapsing neutron stars - specifically what is the ratio created during the beta and r process."
-input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+time_start = datetime.now().strftime("%H:%M:%S")
+print("genarate start: ", datetime.now().strftime("%H:%M:%S"))
 
-outputs = model.generate(**input_ids, max_new_tokens=10000)
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+outputs = model.generate(**input_ids, 
+                         max_new_tokens=10000)
 print(tokenizer.decode(outputs[0]))
+
+print("end", datetime.now().strftime("%H:%M:%S"))
+time_end = datetime.now().strftime("%H:%M:%S")
