6492f31f998f6d3772c7107188fa2cb9335349ed
Michael OBrien <michael@obrienlabs.org>
2024-02-26 18:05:37 -0500

    #13 - CUDA_VISIBLE_DEVICES 0,1 OR 1

--- environments/windows/src/google-gemma/gemma-gpu.py
+++ environments/windows/src/google-gemma/gemma-gpu.py
@@ -1,7 +1,14 @@
+import os
+# default dual GPU - either PCIe bus or NVidia bus - slowdowns
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+# specific GPU - model must fit entierely in memory RTX-3500 ada = 12G, A4000=16G, A4500=20, A6000=48, 4000 ada = 20, 5000 ada = 32, 6000 ada = 48
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from datetime import datetime
 
 #access_token='hf_cfTP...XCQqH'
+
 
 
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b", token=access_token)
