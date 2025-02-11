241763771ac70a49da423a1a2ffcee745523cbb8
Michael O'Brien <michael@obrienlabs.org>
2025-02-10 20:22:42 -0500

    #40 - dtype

--- src/distributed_ml/mnist_setup.py
+++ src/distributed_ml/mnist_setup.py
@@ -14,12 +14,12 @@ def mnist_dataset(batch_size):
 
 def build_and_compile_cnn_model():
   model = tf.keras.Sequential([
-      tf.keras.layers.InputLayer(input_shape=(28, 28)),
-      tf.keras.layers.Reshape(target_shape=(28, 28, 1)),# dtype=tf.float32),
-      tf.keras.layers.Conv2D(32, 3, activation='relu'),
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(128, activation='relu'),
-      tf.keras.layers.Dense(10)
+      tf.keras.layers.InputLayer(input_shape=(28, 28), dtype=tf.float32),
+      tf.keras.layers.Reshape(target_shape=(28, 28, 1), dtype=tf.float32),
+      tf.keras.layers.Conv2D(32, 3, activation='relu', dtype=tf.float32),
+      tf.keras.layers.Flatten(dtype=tf.float32),
+      tf.keras.layers.Dense(128, activation='relu', dtype=tf.float32),
+      tf.keras.layers.Dense(10, dtype=tf.float32)
   ])
   model.compile(
       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
